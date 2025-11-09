cb150f817ee72d20d7230c11193f90a786a5498c
Bún Bò đáng iu <145783665+Ngleanthu@users.noreply.github.com>
2025-11-09 12:57:19 +0700

    config authentication routes with unknownroute

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -2,4 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:bytequeens_adm/config/app_constants.dart';
 class AppRoutes {
   
+  static Route<dynamic> onUnknownRoute(RouteSettings settings) {
+    return MaterialPageRoute(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Page Not Found'),
+        ),
+        body: Center(
+          child: Text('No route defined for ${settings.name}'),
+        ),
+      ),
+    );
+  }
 }
